8b9e93c9cb62094fb255e015c2df06c66ec55025
JonathanDek <jonathanmac00@gmail.com>
2022-04-14 13:17:53 -0700

    Workday models have beeen added, May be subject to change

--- Workday_Management/models.py
+++ Workday_Management/models.py
@@ -1,3 +1,25 @@
+from enum import auto
 from django.db import models
 
-# Create your models here.
+
+
+class Location(models.Model):
+    North="North"
+    East="East"
+    South="South"
+    West="West"
+    Sector_Choices = [(North,'North'),(East,'East'),(South,'South'), (West,'West')]
+    Name = models.CharField(max_length=100)
+    Sector = models.CharField(max_length=5, choices=Sector_Choices)
+
+class TimeCard(models.Model):
+    North="North"
+    East="East"
+    South="South"
+    West="West"
+    Sector_Choices = [(North,'North'),(East,'East'),(South,'South'), (West,'West')]
+    Date = models.DateField()
+    Sector = models.CharField(max_length=5, choices=Sector_Choices)
+    Location = models.CharField(max_length=1000)
+    TimeIn = models.TimeField(auto_now = False, auto_now_add = False)
+    TimeOut = models.TimeField(auto_now = False, auto_now_add = False)
